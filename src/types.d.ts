62d6d0186991c2e30bf7ca3a0d0b6ef222f64849
nozredge <nozredge@proton.me>
2025-09-24 22:48:03 -0300

    edit types.d.ts

---
+++ src/types.d.ts
@@ -0,0 +1,10 @@
+type ET = {
+    element: Element;
+    value: string;
+};
+
+type StringMap<K> = Record<string, K>;
+type DataT = StringMap<string | number>;
+type BindingsT = StringMap<ET>;
+
+export type { BindingsT, DataT };
